9cb76c63a0f400799ded1b8649c2ac26f28dbed6
joshfeds14 <joshfeds14@gmail,com>
2021-06-01 19:03:16 -0400

    Second Exercise

--- src/main/java/oop/example/Exercise02.java
+++ src/main/java/oop/example/Exercise02.java
@@ -1,7 +1,11 @@
 package oop.example;
+import java.util.Scanner;
 
 public class Exercise02 {
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
         System.out.print("What is the input string?");
+        String word = input.next();
+        System.out.printf("%s has %d characters", word, word.length());
     }
 }
