afd30e191f80423ece51c33ef1d9709711a3ba7b
joshfeds14 <joshfeds14@gmail,com>
2021-06-04 16:15:45 -0400

    Second Exercise

--- src/main/java/oop/example/Exercise02.java
+++ src/main/java/oop/example/Exercise02.java
@@ -1,6 +1,9 @@
 package oop.example;
 import java.util.Scanner;
-
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Joshua Federman
+ */
 public class Exercise02 {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
